31a8b6064f3e6a3c9e76a6641a7a95ae92f1b39e
Rajat Ahuja <Rajat_Ahuja@utexas.edu>
2019-10-08 20:28:25 -0500

    added upload capability, fixed dropdown on front page

--- mlapp/src/index.js
+++ mlapp/src/index.js
@@ -2,14 +2,34 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
-import upload from "express-fileupload";
+import upload from 'express-fileupload';
+import bodyParser from 'body-parser'
+import fs from 'fs';
 
 const app = express();
 
 app.use(cors()); //middleware
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(upload())
+
+var directoryPath = path.join(__dirname, 'upload');
+
 
 app.get('/', (req, res) => {
-  res.render('index.ejs');
+  var myData = [];
+  fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        myData.push(file);
+    });
+    res.render('index.ejs', {myData});
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -24,23 +44,21 @@ app.get('/upload', (req, res) => {
   res.render('upload.ejs');
 })
 
-app.post('/', (req, res) => {
+app.post('/upload', (req, res) => {
   if (req.files) {
-    var file = req.files.fileName,
-      filename = req.file.name;
-    file.mv('./upload'+filename, (err) => {
+    var file = req.files.filename,
+      filename = file.name;
+    file.mv('./src/upload/'+filename, (err) => {
       if(err) {
         console.log(err);
-        res.send("error occured");
+        res.send("The file failed to upload");
       }
       else {
-        res.send("Done");
+        res.render('upload.ejs');
       }
     })
   }
 })
-
-
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}!`)
